26825b9422bf73ba69cd8fec6bffe68413eb1ca0
RoxyPursey <roxy.pursey@gmail.com>
2020-04-14 16:28:30 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
+import DisplaySimpson from './Components/DisplaySimpson';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const sampleSimpson = {
+  character: "Bart Simpson",
+  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638",
+  quote: "You have a smelly body and your butt stinks"
+};
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Simpson: sampleSimpson
+    };
+    this.getSimpson = this.getSimpson.bind(this);
+  }
+
+  getSimpson() {
+    // Send the request
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          Simpson: data[0]
+        });
+      });
+  }
+
+  render() {
+    return ( <
+      div className = "App" >
+      <
+      DisplaySimpson Simpson = {
+        this.state.Simpson
+      }
+      /> <
+      button type = "button"
+      onClick = {
+        this.getSimpson
+      } >
+      New Hilarious Quote <
+      /button> < /
+      div >
+    );
+  }
+}
 export default App;
